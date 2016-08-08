5236558507e0410c4ee3d7c909afa8037de50097
xiaoshitou0115@iCloud.com <xiaoshitou0115icloud.com@xiaoshitou0115icloudcomdemacbook-air.local>
2016-08-08 10:08:49 +0800

    新建两个ViewController

---
+++ FirstSVN/FirstSVN/BViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BViewController.swift
+//  FirstSVN
+//
+//  Created by xiaoshitou0115@iCloud.com on 16/8/8.
+//  Copyright © 2016年 ZL. All rights reserved.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print("新建2")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
