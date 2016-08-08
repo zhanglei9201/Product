f05d81a10c12ee2f975cef73297bd0b2ad19e3bb
xiaoshitou0115@iCloud.com <xiaoshitou0115icloud.com@xiaoshitou0115icloudcomdemacbook-air.local>
2016-08-08 09:45:39 +0800

    初始化提交

---
+++ FirstSVN/FirstSVN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSVN
+//
+//  Created by xiaoshitou0115@iCloud.com on 16/8/8.
+//  Copyright © 2016年 ZL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
