2181a81f4bfad385d811b0c80658a561d079938a
xiaoshitou0115@iCloud.com <xiaoshitou0115icloud.com@xiaoshitou0115icloudcomdemacbook-air.local>
2016-08-08 09:58:42 +0800

    修改代码1

--- FirstSVN/FirstSVN/ViewController.swift
+++ FirstSVN/FirstSVN/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a n
+        print("心塞")
     }
 
     override func didReceiveMemoryWarning() {
